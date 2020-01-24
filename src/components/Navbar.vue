79e83783e1401ea6b2d2f672234aa87b72eabed8
joseph walter <wjoey13@gmail.com>
2020-01-24 15:14:47 -0700

    created redirects with a new branch

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -4,6 +4,18 @@
             <li><router-link :to ="{name: 'Home'}">Home</router-link></li>
             <li><router-link :to ="{name: 'About'}">About</router-link></li>
         </ul>
+        <h2>User Profiles</h2>
+        <ul>
+            <li v-for="(id, index) in userIds" :key="index">
+                <router-link :to="{name:'ViewProfile', params:{ user_id: id }}">
+                 <span>Profile {{ id }}</span>
+                </router-link>
+            </li>
+        </ul>
+        <h2>Navagation controls</h2>
+        <ul>
+            <li><button @click="goHome">Redirect to Home</button></li>
+        </ul>
     </nav>
 </template>
 <script>
@@ -11,7 +23,12 @@ export default {
     name: 'Navbar',
     data(){
         return {
-            
+            userIds:['1','2','3','4']
+        }
+    },
+    methods:{
+        goHome(){
+            this.$router.push({name: 'Home'})
         }
     }
 }

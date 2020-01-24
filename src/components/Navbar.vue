e047bea105e479dc4972561d634231b23e801e83
joseph walter <wjoey13@gmail.com>
2020-01-24 16:08:57 -0700

    added to forward and backward button for redirect

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -14,7 +14,9 @@
         </ul>
         <h2>Navagation controls</h2>
         <ul>
+            <li><button @click="goBack">Go Back</button></li>
             <li><button @click="goHome">Redirect to Home</button></li>
+            <li><button @click="goForward">Go Forward</button></li>
         </ul>
     </nav>
 </template>
@@ -29,6 +31,12 @@ export default {
     methods:{
         goHome(){
             this.$router.push({name: 'Home'})
+        },
+        goBack(){
+            this.$router.go(-1)
+        },
+        goForward(){
+            this.$router.go(+1)
         }
     }
 }
@@ -40,5 +48,9 @@ ul{
 }
 a{
     color: 42b983;
+}
+li{
+    display: inline-block;
+    margin: 10px;
 }
 </style>

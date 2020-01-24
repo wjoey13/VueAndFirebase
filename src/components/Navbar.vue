5b5674652f6c083c18919748c1b8306b70a7b518
joseph walter <wjoey13@gmail.com>
2020-01-24 13:20:21 -0700

    Worked on Vue-router and vue-router links

---
+++ src/components/Navbar.vue
@@ -0,0 +1,27 @@
+<template>
+    <nav class="main-navbar">
+        <ul>
+            <li><router-link :to ="{name: 'Home'}">Home</router-link></li>
+            <li><router-link :to ="{name: 'About'}">About</router-link></li>
+        </ul>
+    </nav>
+</template>
+<script>
+export default {
+    name: 'Navbar',
+    data(){
+        return {
+            
+        }
+    }
+}
+</script>
+<style>
+ul{
+    list-style-type: none;
+    padding: 0;
+}
+a{
+    color: 42b983;
+}
+</style>

b3c4108a35434fcdad91cc4f7888fbde4526ca86
joseph walter <wjoey13@gmail.com>
2020-01-24 13:58:56 -0700

    created a route parameter

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/Home'
 import About from '@/components/About'
-
+import ViewProfile from '@/components/ViewProfile'
 Vue.use(Router)
 
 export default new Router({
@@ -15,6 +15,11 @@ export default new Router({
             path: '/about',
             name: 'About',
             component: About
+        },
+        {
+            path: '/profile/:user_id',
+            name: 'ViewProfile',
+            component: ViewProfile
         }
     ]
 
